e2b50275e03de459bcdd50f25a3ef1d70e338913
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-03-11 14:47:28 +0530

    added nav

--- common/components/Navbar/NavLinks/index.tsx
+++ common/components/Navbar/NavLinks/index.tsx
@@ -1,14 +1,18 @@
+"use client";
+
+import { useSelectedLayoutSegments } from "next/navigation";
+
 import Link from "next/link";
 
 const NavLinks = () => {
   const navItems = [
     {
       label: "Movies",
-      href: "/movies",
+      href: "/genre/movies",
     },
     {
       label: "Tv Shows",
-      href: "/tv-shows",
+      href: "/genre/tv-shows",
     },
     {
       label: "My List",
@@ -16,10 +20,18 @@ const NavLinks = () => {
     },
   ];
 
+  const segments = useSelectedLayoutSegments();
+  const currentPath = segments.reduce((prev, curr) => `${prev}/${curr}`, "");
+
   return (
     <ul className="flex items-center gap-4 text-stone-300">
       {navItems.map((navItem) => (
-        <li key={navItem.href}>
+        <li
+          key={navItem.href}
+          className={`${
+            currentPath === navItem.href ? "text-white" : "hover:text-stone-400"
+          } transition-colors`}
+        >
           <Link href={navItem.href}>{navItem.label}</Link>
         </li>
       ))}
