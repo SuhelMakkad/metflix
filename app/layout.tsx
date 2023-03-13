882b409b66cf6ed372879e82e0b1d6c5b3387d2f
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-03-13 13:54:24 +0530

    removed @next/font and using inbuild lib

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/Navbar";
-import { Open_Sans } from "@next/font/google";
+import { Open_Sans } from "next/font/google";
 import "@/css/globals.css";
 
 const openSans = Open_Sans({
