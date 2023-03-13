e587d343fce439e7d5f22c1d6a867641f0cd11cb
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-03-13 23:02:10 +0530

    extracted the nav button to diffretn compoent

--- common/components/Carousel/NavButton/index.tsx
+++ common/components/Carousel/NavButton/index.tsx
@@ -16,15 +16,15 @@ const NavButton = ({ className, direction = "left", ...props }: Props) => {
       {...props}
       className={`
         ${className} absolute top-1/2 z-10 grid h-16 w-16 -translate-y-1/2
-        place-content-center rounded-full bg-black/50 p-4 text-4xl opacity-0
-        transition-all duration-300 focus-within:block hover:bg-black/80
-        focus-visible:translate-x-0 focus-visible:opacity-100
-        group-focus-within/carousel:translate-x-0 group-focus-within/carousel:opacity-100
-        group-hover/carousel:translate-x-0 group-hover/carousel:opacity-100
-        group-focus-visible/carousel:translate-x-0 group-focus-visible/carousel:opacity-100 ${
+        scale-50 place-content-center rounded-full bg-black/50 p-4 text-4xl opacity-0 transition-all
+        duration-300 focus-within:block hover:bg-black/80 focus-visible:translate-x-0
+        focus-visible:opacity-100 group-focus-within/carousel:translate-x-0
+        group-focus-within/carousel:opacity-100 group-hover/carousel:translate-x-0
+        group-hover/carousel:opacity-100 group-focus-visible/carousel:translate-x-0
+        group-focus-visible/carousel:opacity-100 md:scale-75 xl:scale-100 ${
           direction === "right"
-            ? "right-8 origin-right translate-x-32"
-            : "left-8 origin-left -translate-x-32"
+            ? "right-4 origin-right translate-x-32  md:right-6 xl:right-8"
+            : "left-4 origin-left -translate-x-32 md:left-6 xl:left-8"
         }
       `}
     >
