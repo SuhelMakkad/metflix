c83fa6a7ba2d3a344efd4fbf54681d44407a9da2
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-03-21 20:09:41 +0530

    showing portrait image for mobile

--- common/components/BannerSection/index.tsx
+++ common/components/BannerSection/index.tsx
@@ -49,9 +49,18 @@ const BannerSection = () => {
       <Image
         src={`https://image.tmdb.org/t/p/original/${bannerMovie.backdrop_path}`}
         alt={`Image for movie ${bannerMovie.original_title}`}
-        className="-z-50 min-h-[30rem] w-full object-cover"
+        className="-z-50 hidden min-h-[30rem] w-full object-cover md:block"
         height={1152}
         width={2048}
+        property={"true"}
+      />
+
+      <Image
+        src={`https://image.tmdb.org/t/p/original/${bannerMovie.poster_path}`}
+        alt={`Image for movie ${bannerMovie.original_title}`}
+        className="-z-50 block min-h-[30rem] w-full object-cover md:hidden"
+        height={1311}
+        width={874}
         property={"true"}
       />
     </section>
