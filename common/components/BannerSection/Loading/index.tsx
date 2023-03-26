1955526cc5aced65f5f6e354a1e1e081973a7e5a
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-03-26 15:17:07 +0530

    fixed over flow issues with banner loading on mobile

--- common/components/BannerSection/Loading/index.tsx
+++ common/components/BannerSection/Loading/index.tsx
@@ -1,16 +1,17 @@
 const LoadingBanner = () => {
   const descriptionLinesCount = 3;
   return (
-    <section className="relative -mx-8 aspect-[0.667] max-h-[85vh] overflow-hidden md:aspect-[1.778]">
-      <div className="absolute inset-0 px-8">
-        <div className="absolute bottom-1/3 z-10 w-full max-w-2xl">
+    <section className="relative -mx-3 aspect-[0.667] max-h-[85vh] overflow-hidden md:-mx-4 md:aspect-[1.778] lg:-mx-6 2xl:-mx-8">
+      <div className="absolute inset-0">
+        <div className="absolute left-0 right-0 bottom-1/3 z-10 w-[42rem] max-w-full px-8">
           <div
             className="
-              mb-5 h-7 w-3/5 animate-pulse bg-stone-900 font-bold 
-              drop-shadow-[0_25px_25px_rgb(0,0,0)] md:h-9 
-              md:drop-shadow-none lg:h-12 xl:h-14
+            mb-5 h-7 w-3/5 animate-pulse bg-stone-900 font-bold 
+            drop-shadow-[0_25px_25px_rgb(0,0,0)] md:h-9 
+            md:drop-shadow-none lg:h-12 xl:h-14
             "
           />
+          <span className="sr-only">loading...</span>
           <div className="flex flex-col gap-3">
             {Array(descriptionLinesCount)
               .fill(0)
