d3b706fb552ea6ac0e3359595353b94d8bceaa71
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-03-26 06:19:21 +0530

    fixed banner overflow issues

--- common/components/BannerSection/index.tsx
+++ common/components/BannerSection/index.tsx
@@ -66,9 +66,9 @@ const BannerSection = ({ media, type }: Props) => {
   if (!bannerItem) return <span></span>;
 
   return (
-    <section className="relative -mx-8 max-h-[85vh] overflow-hidden">
+    <section className="relative -mx-3 max-h-[85vh] overflow-hidden md:-mx-4 lg:-mx-6 2xl:-mx-8">
       <div className="absolute inset-0">
-        <header className="absolute bottom-1/3 z-10 w-[42rem] max-w-full px-8">
+        <header className="absolute left-0 right-0 bottom-1/3 z-10 w-[42rem] max-w-full px-8">
           <h1
             className="
               mb-2 text-3xl font-bold drop-shadow-[0_25px_25px_rgb(0,0,0)] 
