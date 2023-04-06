c8c1619a11a5bbdb902f8a99d17731d2b15b9b03
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-04-06 18:26:55 +0530

    updated sitemap names

---
+++ app/sitemap/main.xml/route.ts
@@ -0,0 +1,27 @@
+export async function GET(request: Request) {
+  const httpType = "https";
+  const host = request.headers.get("host");
+  const baseUrl = `${httpType}://${host}`;
+
+  const sitemaps = ["/sitemap/pages.xml"];
+
+  const propertiesSitemaps: string[] = [];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${[...sitemaps, ...propertiesSitemaps].map(
+      (sitemap) => `
+        <sitemap>
+          <loc>${baseUrl}${sitemap}</loc>
+        </sitemap>
+      `
+    )}
+  </sitemapindex>
+`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "text/xml",
+    },
+  });
+}
